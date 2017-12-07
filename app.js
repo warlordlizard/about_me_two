4b8c0b57f67433af327ab917081bbb046b85aef6
Katy Robinson <katyorob@gmail.com>
2017-12-07 14:48:02 -0800

    added funtions to the rest of the questions (4-7)

--- app.js
+++ app.js
@@ -60,90 +60,102 @@ function q3() {
 }
 q3();
 
+function q4() {
+  var college = prompt(user +', did Alex go to Harvard?');
+  console.log('college guess:', college);
 
-var college = prompt(user +', did Alex go to Harvard?');
-console.log('college guess:', college);
-
-if (college.toLowerCase() === 'n' || college.toLowerCase() === 'no') {
-  alert('Correct ' + user +'! Alex did NOT go to Harvard, he graduated with his BA in Public Health from UW Seattle');
-  college = true;
-  correct++;
-} else {
-  alert('Actually ' + user + ', Alex graduated with his BA in Public Health from UW Seattle');
-  college = false;
-}
-
-if (offspring === true && military === true && travel === true && college === true) {
-  alert('Dang! Thought I would trip you up there. Harvard might have been a bit of a leap.  You are doing great!! Four Correct!');
-}
-
-var girlfriend = prompt(user +', does Alex have a girlfriend named Caitlan?');
-console.log('girlfriend guess:', girlfriend);
-
-if (girlfriend.toLowerCase() === 'y' || girlfriend.toLowerCase() === 'yes') {
-  alert('Correct ' + user +'! And that is the correct spelling');
-  girlfriend = true;
-  correct++;
-} else {
-  alert('Actually ' + user + ', he does');
-  girlfriend = false;
-}
-
-if (offspring === true && military === true && travel === true && college === true && girlfriend === true) {
-  alert('Wow!!! Are you Alex or a good guesser? 100% so far! Great Job!!!');
-}
-
-var number;
-var counterForSix=0;
-
-
-while (number !== 42) {
-  number = parseInt(prompt(user +' , what is Alex\'s favorite number?'));
-
-  if (number === 42) {
-    alert('Correct! It is because 42 is the meaning of life');
-    counterForSix++;
+  if (college.toLowerCase() === 'n' || college.toLowerCase() === 'no') {
+    alert('Correct ' + user +'! Alex did NOT go to Harvard, he graduated with his BA in Public Health from UW Seattle');
+    college = true;
     correct++;
-    break;
-  } else if (number > 42) {
-    alert('Too high!');
-    counterForSix++;
-  } else if (number < 42) {
-    alert('Too Low!');
-    counterForSix++;
-  } else if (isNaN(number) || number === null) {
-    alert('Please enter a number');
-    counterForSix++;
-  }
-  console.log('number guess:', number);
-
-  if (counterForSix >= 4) {
-    break;
-  }
-}
-
-console.log('tries of question 6:', counterForSix);
-
-var favFood = ['pizza', 'burgers','fries','tacos'];
-var answer = prompt('What is one of Alex\'s favorite foods?');
-var flag;
-
-for (var i = 0; i < 5; i++) {
-  console.log('guess at each iteration', answer);
-
-  if ((answer === favFood[0]) || (answer === favFood[1]) || (answer === favFood[2]) || (answer === favFood[3])) {
-    alert('Correct! Alex\'s favorite foods are: ' + favFood);
-    flag = true;
-    correct++;
-    break;
   } else {
-    alert('Incorrect. Keep trying!');
-    answer = prompt('What is one of Alex\'s favorite foods?');
+    alert('Actually ' + user + ', Alex graduated with his BA in Public Health from UW Seattle');
+    college = false;
+  }
+
+  if (offspring === true && military === true && travel === true && college === true) {
+    alert('Dang! Thought I would trip you up there. Harvard might have been a bit of a leap.  You are doing great!! Four Correct!');
   }
 }
-if (!flag) {
-  alert('Incorrect, Alex\'s favorite foods are: ' + favFood);
+q4();
+
+function q5() {
+  var girlfriend = prompt(user +', does Alex have a girlfriend named Caitlan?');
+  console.log('girlfriend guess:', girlfriend);
+
+  if (girlfriend.toLowerCase() === 'y' || girlfriend.toLowerCase() === 'yes') {
+    alert('Correct ' + user +'! And that is the correct spelling');
+    girlfriend = true;
+    correct++;
+  } else {
+    alert('Actually ' + user + ', he does');
+    girlfriend = false;
+  }
+
+  if (offspring === true && military === true && travel === true && college === true && girlfriend === true) {
+    alert('Wow!!! Are you Alex or a good guesser? 100% so far! Great Job!!!');
+  }
 }
+q5();
+
+
+function q6() {
+  var number;
+  var counterForSix=0;
+
+
+  while (number !== 42) {
+    number = parseInt(prompt(user +' , what is Alex\'s favorite number?'));
+
+    if (number === 42) {
+      alert('Correct! It is because 42 is the meaning of life');
+      counterForSix++;
+      correct++;
+      break;
+    } else if (number > 42) {
+      alert('Too high!');
+      counterForSix++;
+    } else if (number < 42) {
+      alert('Too Low!');
+      counterForSix++;
+    } else if (isNaN(number) || number === null) {
+      alert('Please enter a number');
+      counterForSix++;
+    }
+    console.log('number guess:', number);
+
+    if (counterForSix >= 4) {
+      break;
+    }
+  }
+
+  console.log('tries of question 6:', counterForSix);
+}
+q6();
+
+function q7() {
+  var favFood = ['pizza', 'burgers','fries','tacos'];
+  var answer = prompt('What is one of Alex\'s favorite foods?');
+  var flag;
+
+  for (var i = 0; i < 5; i++) {
+    console.log('guess at each iteration', answer);
+
+    if ((answer === favFood[0]) || (answer === favFood[1]) || (answer === favFood[2]) || (answer === favFood[3])) {
+      alert('Correct! Alex\'s favorite foods are: ' + favFood);
+      flag = true;
+      correct++;
+      break;
+    } else {
+      alert('Incorrect. Keep trying!');
+      answer = prompt('What is one of Alex\'s favorite foods?');
+    }
+  }
+  if (!flag) {
+    alert('Incorrect, Alex\'s favorite foods are: ' + favFood);
+  }
+}
+q7();
 
 console.log('num questions correct:', correct);
 
